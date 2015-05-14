645ca09c5b8a3051bf24a87476da17aecbb0fe87
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-05-14 13:59:31 -0600

    Print out results from max function.

--- basics/max3.cpp
+++ basics/max3.cpp
@@ -20,19 +20,20 @@ inline char const* const& max(char const* const& a, char const* const& b) {
 int main() {
   int a = 7;
   int b = 42;
-  ::max(a, b);
+  std::cout << ::max(a, b) << std::endl;
 
   std::string s = "hey";
   std::string t = "you";
-  ::max(s, t);
+  std::cout << ::max(s, t) << std::endl;
 
   int* p1 = &b;
   int* p2 = &a;
-  ::max(p1, p2);
+  std::cout << ::max(p1, p2) << std::endl;
 
   char const* s1 = "David";
   char const* s2 = "Nico";
-  ::max(s1, s2);
+  std::cout << ::max(s1, s2) << std::endl;
+  ;
 
   return 0;
 }
