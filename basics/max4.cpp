65adf40567bada389f223b1078987591f1d88136
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-05-14 20:31:24 -0600

    Add program where not all overloaded functions are visible when a corresponding function call is made.

---
+++ basics/max4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Minimum of two values of any type. Misnamed max.
+template <typename T> inline T const& max(T const& a, T const& b) {
+  return a < b ? a: b;
+}
+
+// Maximum of three values of any type.
+template <typename T> inline T const& max(T const& a, T const& b, T const& c) {
+  return max(max(a, b), c); // Uses the template version even for ints because
+                            // the following declaration comes too late.
+}
+
+// Maximum of two int values
+inline int const& max(int const& a, int const& b) { return a < b ? b : a; }
+
+int main() {
+  std::cout << ::max(0, 7, 42) << std::endl;
+  return 0;
+}
