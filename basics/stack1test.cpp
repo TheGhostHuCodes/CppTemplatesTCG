71f95bab3cdaff963aad1c9360fedb887c8a53e9
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-05-15 07:54:49 -0600

    Add Stack<> template class.

---
+++ basics/stack1test.cpp
@@ -0,0 +1,25 @@
+#include "stack1.hpp"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <stdexcept>
+
+int main() {
+  try {
+    Stack<int> intStack;
+    Stack<std::string> stringStack;
+
+    // manipulate int stack.
+    intStack.push(7);
+    std::cout << intStack.top() << std::endl;
+    // manipulate string stack.
+    stringStack.push("hello");
+    std::cout << stringStack.top() << std::endl;
+    stringStack.pop();
+    stringStack.pop();
+  } catch (std::exception const& ex) {
+    std::cerr << "Exception: " << ex.what() << std::endl;
+    return EXIT_FAILURE; // exit program with ERROR status.
+  }
+  return 0;
+}
