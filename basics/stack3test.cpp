cd05444ac1c34646acf541d1961b13eeda901e11
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-05-15 22:10:18 -0600

    Add Stack class template with default template argument.

---
+++ basics/stack3test.cpp
@@ -0,0 +1,25 @@
+#include "stack3.hpp"
+#include <iostream>
+#include <deque>
+#include <cstdlib>
+#include <stdexcept>
+
+int main() {
+  try {
+    Stack<int> intStack;
+    Stack<double, std::deque<double>> doubleStack;
+
+    // manipulate int stack.
+    intStack.push(7);
+    std::cout << intStack.top() << std::endl;
+    // manipulate string stack.
+    doubleStack.push(42.42);
+    std::cout << doubleStack.top() << std::endl;
+    doubleStack.pop();
+    doubleStack.pop();
+  } catch (std::exception const& ex) {
+    std::cerr << "Exception: " << ex.what() << std::endl;
+    return EXIT_FAILURE; // exit program with ERROR status.
+  }
+  return 0;
+}
