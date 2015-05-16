7994b67270c25e8ab2122b98b135d9378bb7bae6
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-05-15 22:55:55 -0600

    Update build directory location for project.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -95,7 +95,7 @@ flags = [
 #
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = '/Users/yhu/Repos/CppTemplates/build'
+compilation_database_folder = '/Users/yhu/Repos/CppTemplatesTCG/build'
 
 if os.path.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
