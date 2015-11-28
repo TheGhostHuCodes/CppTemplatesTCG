4c45590a0caa052e67bc328a73f0a4758ba8ee13
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-11-28 00:09:54 -0800

    Print results from using max function to console

--- basics/max2.cpp
+++ basics/max2.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 inline int const& max(int const& a, int const& b) { return a < b ? b : a; }
 
 template <typename T> inline T const& max(T const& a, T const& b) {
@@ -9,14 +11,14 @@ template <typename T> inline T const& max(T const& a, T const& b, T const& c) {
 }
 
 int main() {
-  ::max(7, 42, 68);
-  ::max(7.0, 42.0);
-  ::max('a', 'b');
-  ::max(7, 42);
-  ::max<>(7, 42);
-  ::max<double>(7, 42);
-//  ::max('a', 42.7);
-  ::max('a', static_cast<int>(42.7));
+  std::cout << ::max(7, 42, 68) << std::endl;
+  std::cout << ::max(7.0, 42.0) << std::endl;
+  std::cout << ::max('a', 'b') << std::endl;
+  std::cout << ::max(7, 42) << std::endl;
+  std::cout << ::max<>(7, 42) << std::endl;
+  std::cout << ::max<double>(7, 42) << std::endl;
+  // std::cout << ::max('a', 42.7) << std::endl;
+  std::cout << ::max('a', static_cast<int>(42.7)) << std::endl;
 
   return 0;
 }
